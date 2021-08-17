d019eb371cd6dae630d7dd8831414c3377195ec8
M. Caner Özkan <canerozkan513@gmail.com>
2021-08-17 18:02:16 +0300

    components

--- Frontend/AuctionApp/src/app/app.module.ts
+++ Frontend/AuctionApp/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    UsersComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule
